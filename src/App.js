f8e316b679105719c2a3f07e84475b43a702f34d
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2020-05-26 20:58:01 +0530

    changing the title of app

--- src/App.js
+++ src/App.js
@@ -36,54 +36,52 @@ function App() {
   };
 
   return (
-    <div>
-      <div className="row my-5 py-5">
-        <div className="col-md-8 mx-auto">
-          <Card className="text-center shadow">
-            <Card.Header>
-              <h3>Event Countdown</h3>
-            </Card.Header>
-            <Card.Body>
-              <Form>
-                <Row className="mx-auto">
-                  <Col sm="12" className="my-1" lg="6">
-                    <Form.Control
-                      name="EventName"
-                      onChange={(event) => {
-                        setEventName(event.target.value);
-                      }}
-                      placeholder="Enter Event Name"
-                    />
-                  </Col>
-                  <Col sm="12" className="my-1" lg="6">
-                    <ReactDatePicker
-                      placeholderText="Select Event Date"
-                      className="form-control"
-                      name="EventDate"
-                      selected={selectedDate}
-                      onChange={(date) => {
-                        setSelectedDate(date);
-                      }}
-                      minDate={new Date()}
-                      dateFormat="dd/MM/yyyy"
-                    />
-                  </Col>
-                </Row>
-              </Form>
-              <hr />
-              <Card.Title>
-                <h2>{eventName}</h2>
-              </Card.Title>
-              <Card.Text>
-                Days: {timer.days}, Hours: {timer.hours}, Minutes:{" "}
-                {timer.minutes}, Seconds: {timer.seconds}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer className="blockquote-footer">
-              Chetan Bhogade
-            </Card.Footer>
-          </Card>
-        </div>
+    <div className="row my-5 py-5">
+      <div className="col-md-8 mx-auto">
+        <Card className="text-center shadow">
+          <Card.Header>
+            <h3>Event Countdown</h3>
+          </Card.Header>
+          <Card.Body>
+            <Form>
+              <Row className="mx-auto">
+                <Col sm="12" className="my-1" lg="6">
+                  <Form.Control
+                    name="EventName"
+                    onChange={(event) => {
+                      setEventName(event.target.value);
+                    }}
+                    placeholder="Enter Event Name"
+                  />
+                </Col>
+                <Col sm="12" className="my-1" lg="6">
+                  <ReactDatePicker
+                    placeholderText="Select Event Date"
+                    className="form-control"
+                    name="EventDate"
+                    selected={selectedDate}
+                    onChange={(date) => {
+                      setSelectedDate(date);
+                    }}
+                    minDate={new Date()}
+                    dateFormat="dd/MM/yyyy"
+                  />
+                </Col>
+              </Row>
+            </Form>
+            <hr />
+            <Card.Title>
+              <h2>{eventName}</h2>
+            </Card.Title>
+            <Card.Text>
+              Days: {timer.days}, Hours: {timer.hours}, Minutes: {timer.minutes}
+              , Seconds: {timer.seconds}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer className="blockquote-footer">
+            Chetan Bhogade
+          </Card.Footer>
+        </Card>
       </div>
     </div>
   );
