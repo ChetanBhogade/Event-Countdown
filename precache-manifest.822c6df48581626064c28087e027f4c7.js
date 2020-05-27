744a6b7a47294567a450fbc4fee55d4c4e024492
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2020-05-27 12:50:47 +0530

    Updates

---
+++ precache-manifest.822c6df48581626064c28087e027f4c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34dc814923c8939c2d32daf5aec31534",
+    "url": "/Event-Countdown/index.html"
+  },
+  {
+    "revision": "a6596f3922cf278a1c2b",
+    "url": "/Event-Countdown/static/css/2.06b17916.chunk.css"
+  },
+  {
+    "revision": "03a51e8f1b3173cbc680",
+    "url": "/Event-Countdown/static/css/main.2ee823b3.chunk.css"
+  },
+  {
+    "revision": "a6596f3922cf278a1c2b",
+    "url": "/Event-Countdown/static/js/2.bae27f63.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Event-Countdown/static/js/2.bae27f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03a51e8f1b3173cbc680",
+    "url": "/Event-Countdown/static/js/main.a151a9ea.chunk.js"
+  },
+  {
+    "revision": "4631622ab222c2e1a185",
+    "url": "/Event-Countdown/static/js/runtime-main.61269a07.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53d95032ae8375dbb8d61435532429d8.js"
+  "/Event-Countdown/precache-manifest.822c6df48581626064c28087e027f4c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Event-Countdown/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
